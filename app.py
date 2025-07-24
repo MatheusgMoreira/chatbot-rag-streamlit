5a39f55189da543a82b4633f25a9011e48848d24
Matheus Moreira <75402234+MatheusgMoreira@users.noreply.github.com>
2025-07-24 15:10:23 -0300

    Versão 8 - 24/07/2025

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import gdown
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings

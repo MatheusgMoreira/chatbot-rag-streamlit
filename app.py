d25f7b3e8197ce36d7c6d562fbc48077a1df70cc
Matheus Moreira <75402234+MatheusgMoreira@users.noreply.github.com>
2025-07-24 13:58:54 -0300

    Versão 7 - 24/07/2025

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import os
 @st.cache_resource
 def setup_database():
     PERSIST_DIRECTORY = 'chroma_db_gemini'
-    DB_ZIP_URL = "https://drive.usercontent.google.com/download?id=1evAv8YK5AiDBdnrAYJTFmEVwcD67cHjH&export=download&authuser=0"
+    DB_ZIP_URL = "https://github.com/MatheusgMoreira/chatbot-rag-streamlit/releases/download/v1.0.0/chroma_db_gemini.zip"
     
     if not os.path.exists(PERSIST_DIRECTORY):
         st.info("Base de dados não encontrada. Baixando e configurando...")
